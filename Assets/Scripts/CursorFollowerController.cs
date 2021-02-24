dc24cdcc5d3c58a9d07379b0f6ba0f68885b7c5c
ahSOLO <junmenghan@gmail.com>
2021-02-23 22:23:27 -0800

    add cursor follower, blended camera

---
+++ Assets/Scripts/CursorFollowerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFollowerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
